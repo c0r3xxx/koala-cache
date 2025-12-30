a960838b92f7c34a8517e8da5c8bf22d5658ef23
c0r3xxx <aaron1324@gmx.net>
2025-12-30 21:34:01 +0100

    db connectivity

---
+++ xbackend/src/db/init.rs
@@ -0,0 +1,12 @@
+use sqlx::{PgPool, postgres::PgPoolOptions};
+use std::env;
+
+pub async fn init() -> PgPool {
+    let database_url = env::var("DSN").expect("DSN must be set in .env file");
+
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to Postgres")
+}
--- xbackend/src/main.rs
+++ xbackend/src/main.rs
@@ -1,9 +1,13 @@
+mod db;
 mod img;
 mod routes;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    let db_pool = db::init().await;
+    println!("Database connected successfully!");
 
     routes::init().await;
 }
